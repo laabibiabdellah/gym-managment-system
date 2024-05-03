0be8baefa053b5ca5b9791815beb31bf5c6c1936
abdellah <admirablea42@gmail.com>
2024-05-03 20:02:55 +0200

    add font awesome cdn link

--- resources/views/components/admin-dashboard.blade.php
+++ resources/views/components/admin-dashboard.blade.php
@@ -19,10 +19,7 @@
     <link rel="stylesheet" href="{{asset('css/pages/admin/style.css')}}"></head>
 
         <!-- Font Awesome -->
-        <link
-        href="{{asset('assets/all.min.css')}}"
-        rel="stylesheet"
-        />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- Google Fonts -->
         <link
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
